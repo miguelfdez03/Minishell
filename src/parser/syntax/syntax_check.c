88023ff24985767d284d8a5b2e432951546a3812
Miguel <miguelfm03@gmail.com>
2025-11-09 20:15:07 +0100

    fix

--- src/parser/syntax/syntax_check.c
+++ src/parser/syntax/syntax_check.c
@@ -1,24 +1,38 @@
->>>>>>> origin/executor
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "../../minishell.h"
+static int	check_pipe_syntax(t_token *current)
+{
+	if (!current->next)
+	{
+		ft_putendl_fd("minishell: syntax error near unexpected token `|'", 2);
+		return (0);
+	}
+	if (current->next->type == PIPE)
+	{
+		ft_putendl_fd("minishell: syntax error near unexpected token `|'", 2);
+		return (0);
+	}
+	return (1);
+}
 
 int	check_syntax(t_data **data)
 {
-	t_token	*tokens;
+	t_token	*current;
 
-	tokens = (*data)->tokens;
-<<<<<<< HEAD
-	t_token	*tokens;
-
-	tokens = (*data)->tokens;
-	if (tokens->type == PIPE)
-		return (EXIT_FAILURE);
+	if (!data || !(*data) || !(*data)->tokens)
+		return (1);
+	current = (*data)->tokens;
+	if (current->type == PIPE)
+	{
+		ft_putendl_fd("minishell: syntax error near unexpected token `|'", 2);
+		return (0);
+	}
+	while (current)
+	{
+		if (current->type == PIPE)
+		{
+			if (!check_pipe_syntax(current))
+				return (0);
+		}
+		current = current->next;
+	}
+	return (1);
 }
-
-=======
-	if (tokens->type == PIPE)
-		return (EXIT_FAILURE);
-}
->>>>>>> origin/executor
